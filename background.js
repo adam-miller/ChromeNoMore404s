98b2c682510f578e83425eb8e59e7a296558e520
Adam Miller <adam.spokane@gmail.com>
2013-10-11 15:55:49 -0700

    Code cleanup

--- background.js
+++ background.js
@@ -1,26 +1,36 @@
-
-console.log("events registered");
+//Enable by default, if we've never been set before
+chrome.storage.sync.get('isEnabled', function(values){
+	if(typeof values.isEnabled === "undefined") {
+	    chrome.storage.sync.set({'isEnabled': true},function(){
+		});
+	}
+    });
 
 
 chrome.webRequest.onCompleted.addListener(function(details) {
 	if(details.statusCode==404 &&details.type=="main_frame" ) {
 	    console.log(details);
-	    
+
 	    chrome.tabs.get(details.tabId, function(tab) {
 			if(!tab)
 			    console.log('tab is gone');
 			else {
-			    //console.log(tab);
-			    if(tab.status=="loading") {
-				chrome.tabs.onUpdated.addListener(function (id, changeInfo, tab) {
-					if(id==details.tabId && changeInfo.status=="complete")
-					    chrome.tabs.executeScript(details.tabId, {file:"content_script.js"});
-					
-				    });
-			    }
-			    else
-				chrome.tabs.executeScript(details.tabId, {file:"content_script.js"}); 
-			    console.log("script should have been executed");
+			    //Time to run the content script, but only if we are enabled
+			    chrome.storage.sync.get('isEnabled', function(values){
+				    //console.log(values);
+				    if(typeof values.isEnabled === "undefined" || values.isEnabled) {
+					if(tab.status=="loading") {
+					    chrome.tabs.onUpdated.addListener(function (id, changeInfo, tab) {
+						    if(id==details.tabId && changeInfo.status=="complete")
+							chrome.tabs.executeScript(details.tabId, {file:"content_script.js"});
+						    
+						});
+					}
+					else
+					    chrome.tabs.executeScript(details.tabId, {file:"content_script.js"}); 
+					console.log("script should have been executed");
+				    }
+				});
 			}
 
 		    });
@@ -29,27 +39,3 @@ chrome.webRequest.onCompleted.addListener(function(details) {
 
 	    }
     },{urls:["<all_urls>"]});
-
-
-//not called at the moment
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	console.log(sender.tab ?
-		    "from a content script:" + sender.tab.url :
-		    "from the extension");
-
-	if (request.wayback == "isAvailable") {
-
-	    var xhr = new XMLHttpRequest();
-	    xhr.open("GET","http://archive.org/wayback/available?url="+sender.tab.url,true);
-	    //xhr.open("GET","http://archive.org/wayback/available?url=example.com",true);
-	    xhr.onreadystatechange = function() {
-		console.log(xhr.readyState);
-		if(xhr.readyState == 4) {
-		    var resp = JSON.parse(xhr.responseText);
-		    //sendResponse(resp);
-		}
-	    }
-	    //xhr.send();
-	    //return true;
-	}
-  });
--- content_script.js
+++ content_script.js
@@ -1,26 +1,17 @@
 
-//old message passing code
-//chrome.runtime.sendMessage({wayback: "isAvailable"}, function(response) {
-//if(!response)
-//console.log("background doesn't exist");
-//else {
-//console.log(response);
-//console.log(response.archived_snapshots);
-//}
-//});
 
 
 var xhr = new XMLHttpRequest();
-console.log("creating xhr");
+//console.log("creating xhr");
 xhr.open("GET","http://archive.org/wayback/available?url="+window.location,true);
 //xhr.open("GET","http://archive.org/wayback/available?url=example.com",true);
-console.log("xhr now open");
+//console.log("xhr now open");
 xhr.onreadystatechange = function() {
-    console.log(xhr.readyState);
+    //console.log(xhr.readyState);
     if(xhr.readyState == 4) {
 	var resp = JSON.parse(xhr.responseText);
 	//sendResponse(resp);
-	console.log(resp);
+	//console.log(resp);
 	WM_loadBanner(resp);
     }
 }
@@ -34,13 +25,13 @@ function WM_loadBanner(response) {
        response.archived_snapshots.closest.available==true && 
        response.archived_snapshots.closest.status.indexOf("2")==0) {
 	
-	console.log(response.archived_snapshots.closest.timestamp);
+	//console.log(response.archived_snapshots.closest.timestamp);
 	
 	var bodyElement = document.getElementsByTagName("body")[0];
 	var banner = document.createElement("div");
 	var text = document.createTextNode("Here is a link to an older version on archive.org - ");
 	banner.setAttribute("style","display: block; position: relative; z-index: 99999; border: 1px solid; color: black; background-color: rgb(255, 255, 224); font-size: 15px; font-family: sans-serif; padding: 5px; ");
-	console.log(banner.style);
+	//console.log(banner.style);
 	banner.appendChild(text);
 	var newLink = document.createElement("a");
 	newLink.href="http://web.archive.org/web/"+response.archived_snapshots.closest.timestamp+"/"+window.location;
